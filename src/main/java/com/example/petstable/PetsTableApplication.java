2a447ef2a349ad4e16853e52412d0a9963e02616
Seung-9 <ssg9505fj22@naver.com>
2024-04-06 16:00:25 +0900

    초기 세팅

---
+++ src/main/java/com/example/petstable/PetsTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.petstable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetsTableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petstable/PetsTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petstable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
