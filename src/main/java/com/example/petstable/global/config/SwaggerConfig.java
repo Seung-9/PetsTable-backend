984a52e9fc00c9152180c27767a29c9189b059cb
Seung-9 <ssg9505fj22@naver.com>
2024-04-08 13:06:53 +0900

    fix: https 환경에서 swagger 이용 가능하도록 변경

--- src/main/java/com/example/petstable/global/config/SwaggerConfig.java
+++ src/main/java/com/example/petstable/global/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.example.petstable.global.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -10,24 +11,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
+import java.util.List;
 @OpenAPIDefinition(
+        servers = @Server(url = "/", description = "Default Server URL"),
         info = @Info(
-                title = "백엔드 API 명세",
+                title = "PetsTable 백엔드 API 명세",
                 description = "springdoc을 이용한 Swagger API 문서입니다.",
                 version = "1.0",
                 contact = @Contact(
                         name = "springdoc 공식문서",
                         url = "https://springdoc.org/"
-                )
-        )
+                ))
 )
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .components(new Components().addSecuritySchemes("JWT", bearerAuth()));
+        return new OpenAPI().components(new Components().addSecuritySchemes("JWT", bearerAuth()))
+                .servers(List.of());
     }
 
     public SecurityScheme bearerAuth() {
